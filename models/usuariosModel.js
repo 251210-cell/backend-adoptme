6a441acb74138b3f28d1d2e1faaaa4d6f68df163
karla <cruzjimenezkarla90@gmail.com>
2026-04-16 03:26:12 -0600

    chat en solis

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -55,4 +55,10 @@ const Usuario = sequelize.define('Usuario', {
   }
 });
 
+// --- ESTO ES LO QUE FALTA ---
+// Le avisamos a Usuario que puede tener muchas solicitudes
+Usuario.associate = (models) => {
+  Usuario.hasMany(models.Solicitud, { foreignKey: 'id_usuario', as: 'solicitudes' });
+};
+
 module.exports = Usuario;
